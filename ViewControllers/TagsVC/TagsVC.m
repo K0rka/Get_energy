62565a9054f691d55eb329d43a1c31dedf83709c
Catherine Korovkina <c.korovkina@gmail.com>
2014-07-05 19:04:12 +0400

    Adding and deleting tags

--- ViewControllers/TagsVC/TagsVC.m
+++ ViewControllers/TagsVC/TagsVC.m
@@ -11,6 +11,7 @@
 #import <NSManagedObject+MagicalAggregation.h>
 #import <NSManagedObject+MagicalRequests.h>
 #import "TagsCell.h"
+#import "UIAlertView+Blocks.h"
 
 @interface TagsVC ()
 @property (nonatomic, strong) NSFetchedResultsController *frc;
@@ -39,6 +40,9 @@
                                                     managedObjectContext:[NSManagedObjectContext MR_defaultContext]
                                                       sectionNameKeyPath:nil
                                                                cacheName:@"Tags"];
+    [self.frc performFetch:nil];
+
+    self.frc.delegate = self;
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
@@ -72,44 +76,37 @@
 }
 
 
-/*
+
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
+
+
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+        __weak TagsVC *wSelf = self;
+        [[NSManagedObjectContext MR_defaultContext] deleteObject:[wSelf.frc objectAtIndexPath:indexPath]];
+        [[NSManagedObjectContext MR_defaultContext ] save:nil];
+//        [MagicalRecord saveWithBlock:^(NSManagedObjectContext *localContext) {
+//            [[] deleteObject:[wSelf.frc objectAtIndexPath:indexPath]];
+//        }];
+    }
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return UITableViewCellEditingStyleDelete;
 }
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
+- (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return NSLocalizedString(@"Delete", nil);
 }
-*/
-
 /*
 #pragma mark - Navigation
 
@@ -120,5 +117,93 @@
     // Pass the selected object to the new view controller.
 }
 */
+- (IBAction)addNewTag:(id)sender {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"New Tag", nil) message:@"New Tag Name" cancelButtonItem:[RIButtonItem itemWithLabel:NSLocalizedString(@"Cancel", nil)] otherButtonItems:nil];//initWithTitle: message: delegate:self cancelButtonTitle: otherButtonTitles:nil];
+    
+    RIButtonItem *okItem = [RIButtonItem itemWithLabel:NSLocalizedString(@"OK", nil) action:^{
+        
+        UITextField *textField = [alertView textFieldAtIndex:0];
+        if (textField.text.length) {
+            [MagicalRecord saveWithBlock:^(NSManagedObjectContext *localContext) {
+                Tag *newTag = [Tag MR_createInContext:localContext];
+                newTag.tagName = textField.text;
+                
+            }
+             completion:^(BOOL success, NSError *error) {
+                 
+                 if (success) {
+                     
+                 }
+                 else if (error) {
+                     NSLog(@"error = %@", error);
+                 }
+             }];
+        }
+    }];
+    [alertView addButtonItem:okItem];
+    alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+    [alertView show];
+}
+
+
+
+#pragma mark FRC
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView beginUpdates];
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+    
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex]
+                          withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex]
+                          withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath {
+    
+    UITableView *tableView = self.tableView;
+    
+    switch(type) {
+            
+        case NSFetchedResultsChangeInsert:
+            [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath]
+                             withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                             withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeUpdate:
+            [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            break;
+            
+        case NSFetchedResultsChangeMove:
+            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                             withRowAnimation:UITableViewRowAnimationFade];
+            [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath]
+                             withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView endUpdates];
+}
 
 @end
